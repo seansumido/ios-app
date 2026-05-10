859db3a206aa63dcde5c69028184add6799b7d7e
mspecimendev <mspecimen.dev@gmail.com>
2026-05-11 01:45:44 +0800

    init

---
+++ www/js/app.js
@@ -0,0 +1,50 @@
+var $ = Dom7;
+
+var device = Framework7.getDevice();
+var app = new Framework7({
+  name: 'My iOS App 2', // App name
+  theme: 'auto', // Automatic theme detection
+  colors: {
+    primary: '#ceaa42',
+  },
+
+  el: '#app', // App root element
+
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+
+
+
+  // Input settings
+  input: {
+    scrollIntoViewOnFocus: device.cordova,
+    scrollIntoViewCentered: device.cordova,
+  },
+  // Cordova Statusbar settings
+  statusbar: {
+    iosOverlaysWebView: true,
+    androidOverlaysWebView: false,
+  },
+  on: {
+    init: function () {
+      var f7 = this;
+      if (f7.device.cordova) {
+        // Init cordova APIs (see cordova-app.js)
+        cordovaApp.init(f7);
+      }
+    },
+  },
+});
+// Login Screen Demo
+$('#my-login-screen .login-button').on('click', function () {
+  var username = $('#my-login-screen [name="username"]').val();
+  var password = $('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
+});
